ad20687bf4e6c37609c206f44a7b93223d5144f2
pasci199601815 <pascal.madlmayr@gmail.com>
2022-05-31 23:41:11 +0200

    updated test file for db connection

--- main_test.go
+++ main_test.go
@@ -19,9 +19,12 @@ var a App
 
 func TestMain(m *testing.M) {
 	a.Initialize(
-		os.Getenv("APP_DB_USERNAME"),
-		os.Getenv("APP_DB_PASSWORD"),
-		os.Getenv("APP_DB_NAME"))
+		"postgres",
+		"postgres",
+		"postgres")
+		//os.Getenv("APP_DB_USERNAME"),
+		//os.Getenv("APP_DB_PASSWORD"),
+		//os.Getenv("APP_DB_NAME"))
 
 	ensureTableExists()
 	code := m.Run()
